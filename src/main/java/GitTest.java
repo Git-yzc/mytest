2a6da18417d181e088e300288fa075d7e5a0ff2e
Yzc <yangzhaoc079@gmail.com>
2022-04-29 14:35:26 +0800

    GitTestFirstCommit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author: YZC
+ * @date: 2022/4/29 14:22
+ * @description:
+ * @since:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
